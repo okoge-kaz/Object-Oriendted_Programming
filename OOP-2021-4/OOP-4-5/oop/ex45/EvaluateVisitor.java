627d792ddf6484f5312da7ee898f888535c5b632
kazuki <kaz.tokyo.tech20@gmail.com>
2022-01-25 21:42:49 +0900

    fix

--- OOP-2021-4/OOP-4-5/oop/ex45/EvaluateVisitor.java
+++ OOP-2021-4/OOP-4-5/oop/ex45/EvaluateVisitor.java
@@ -8,4 +8,29 @@ public class EvaluateVisitor implements Visitor {
     e.accept(this);
     return v;
   }
+
+  @Override
+  public void visit(Number e){
+    this.v = new Number(e.getValue());
+  }
+
+  @Override
+  public void visit(Bool e){
+    this.v = new Bool(e.getBool());
+  }
+
+  @Override
+  public void visit(Addition e){
+    this.v = new Number(e.getLeftExpression().evaluate().getNumber() + e.getRightExpression().evaluate().getNumber());
+  }
+
+  @Override
+  public void visit(LessThan e){
+    this.v = new Bool(e.getLeftExpression().evaluate().getNumber() < e.getRightExpression().evaluate().getNumber());
+  }
+
+  @Override
+  public void visit(IfThenElse e){
+    this.v = e.getConditional().evaluate().getBool() ? e.getThenPart().evaluate() : e.getElsePart().evaluate();
+  }
 }
